72c74c905969b5f5fb8e3c1b39cd626ea64e0949
Billy <whb227@163.com>
2019-05-13 00:29:40 +0800

    task 2

--- 002_syscall/stdbilly/write_demo.c
+++ 002_syscall/stdbilly/write_demo.c
@@ -3,13 +3,7 @@
 int main() {
     char x[] = "Hello world\n";
 
-#if defined(__i386__)
-    __asm("movl $4, %%eax\n"
-            "movl $1, %%ebx\n"
-            "movl %0, %%ecx\n"
-            "movl $12, %%edx\n"
-            "int $0x80"::"r"(x):"%eax", "%ebx", "%ecx", "%edx");
-#elif defined(__x86_x64__) || defined(__amd64__)
+#if defined(__x86_x64__) || defined(__amd64__)
     __asm__ ("movq $1, %%rax;\n"
             "movq $1, %%rdi;\n"
             "movq %0, %%rsi;\n"
