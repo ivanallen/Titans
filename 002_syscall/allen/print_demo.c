483be609fe29f6596cbc683c34cfff2bfe05c2ea
sswhg <2458772109@qq.com>
2019-05-15 09:50:40 -0700

    task2

--- 002_syscall/allen/print_demo.c
+++ 002_syscall/allen/print_demo.c
@@ -1,7 +1,8 @@
 #include <unistd.h>
 
-int main() {
-    char buf[] = "Hello world\n";
-    write(STDOUT_FILENO, buf, sizeof(buf));
+int main() 
+{
+    char buf[] ="sswhg\n";
+    write(STDOUT_FILENO, buf, sizeof(buf));//读取用户的键盘输入信息
     return 0;
 }
--- 002_syscall/allen/write_demo.c
+++ 002_syscall/allen/write_demo.c
@@ -1,23 +1,7 @@
-// x64 系统调用表
-// https://blog.rchapman.org/posts/Linux_System_Call_Table_for_x86_64/
-// https://filippo.io/linux-syscall-table/
-
-// x86 系统调用表
-// https://syscalls.kernelgrok.com/
-// https://en.wikibooks.org/wiki/X86_Assembly/Interfacing_with_Linux
-
-// How to invoke system call in inline assembly?
-// https://stackoverflow.com/questions/9506353/how-to-invoke-a-system-call-via-sysenter-in-inline-assembly
-
-// how to use sysenter under linux
-// https://reverseengineering.stackexchange.com/questions/2869/how-to-use-sysenter-under-linux
-
-// Pre-defined Compiler Macros
-// https://sourceforge.net/p/predef/wiki/Architectures/
 #include <stdio.h>
 
 int main() {
-    char x[] = "Hello world\n";
+    char x[] = "sswhg\n";
 
 #if defined(__i386__)
     __asm("movl $4, %%eax\n"
