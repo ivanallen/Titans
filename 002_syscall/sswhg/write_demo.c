e34e9fdcc1e3def3330eb728167ad5552a09d3a0
sswhg <2458772109@qq.com>
2019-05-15 23:10:22 -0700

    task2

--- 002_syscall/sswhg/write_demo.c
+++ 002_syscall/sswhg/write_demo.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    char x[] = "sswhg\n";
+    char x[] = "Hello sswhg\n";
 
-#if defined(__x86_x64__) || defined(__amd64__)
-    __asm__ ("movq $1, %%rax;\n"
-            "movq $1, %%rdi;\n"
-            "movq %0, %%rsi;\n"
-            "movq $12, %%rdx;\n"
-            "syscall"::"r"(x):"%rax","%rdi","%rsi","%rdx");
+#if defined(__i386__)
+    __asm("movl $4, %%eax\n"
+            "movl $1, %%ebx\n"
+            "movl %0, %%ecx\n"
+            "movl $12, %%edx\n"
+            "int $0x80"::"r"(x):"%eax", "%ebx", "%ecx", "%edx");
 #endif
     return 0;
 }
