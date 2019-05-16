741a724172db14bdb1c5a925ea66e51eb8c00adb
sswhg <2458772109@qq.com>
2019-05-15 23:29:30 -0700

    task2

--- 002_syscall/sswhg/write_demo.c
+++ 002_syscall/sswhg/write_demo.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    char x[] = "Hello sswhg\n";
+    char x[] = "sswhg\n";
 
 #if defined(__i386__)
     __asm("movl $4, %%eax\n"
             "movl $1, %%ebx\n"
             "movl %0, %%ecx\n"
-            "movl $12, %%edx\n"
+            "movl $6, %%edx\n"
             "int $0x80"::"r"(x):"%eax", "%ebx", "%ecx", "%edx");
 #endif
     return 0;
