695dbbaffc7b1e9aa1e7057d71b2044e357f865f
happyjacob123 <49192984+happyjacob123@users.noreply.github.com>
2019-05-17 22:13:43 +0800

    Add files via upload

--- skx/write_demo.c
+++ skx/write_demo.c
@@ -2,14 +2,13 @@
 
 int main() {
     char x[] = "skx\n";
+
 #if defined(__x86_x64__) || defined(__amd64__)
     __asm__ ("movq $1, %%rax;\n"
             "movq $1, %%rdi;\n"
             "movq %0, %%rsi;\n"
-            "movq $7, %%rdx;\n"
+            "movq $6, %%rdx;\n"
             "syscall"::"r"(x):"%rax","%rdi","%rsi","%rdx");
-#else
-    printf("Unsupported Platform\n");
 #endif
     return 0;
 }
