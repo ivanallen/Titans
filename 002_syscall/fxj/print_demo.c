f742aa3751ad326af2b0aaf42ecd984be98e1582
1364909601 <1364909601@qq.com>
2019-05-18 07:56:50 +0800

    trans into chinese

--- 002_syscall/fxj/print_demo.c
+++ 002_syscall/fxj/print_demo.c
@@ -2,9 +2,9 @@
 int main() {
     char buf[] = "Hello, fuxiangjie!\n";
     write(STDOUT_FILENO, buf, sizeof(buf));
-    // arg1: file descriptor
-    // arg2: a pointer, point to a buf that stores what we're going to write.
-    // arg3: the number of bytes we're going to write.  
+    // arg1: 表示屏幕的代码
+    // arg2: 代表将要打印出来的字符串数组
+    // arg3: 数组所占内存大小  
     // return value: on success, the number of bytes written is returned. On error, return -1.
     return 0;
 }
