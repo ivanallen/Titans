5a69d8235cfc08f61a9b26f7c652b94afe56c24c
hjhlw <164325338@qq.com>
2019-05-19 01:20:09 -0700

    task3

---
+++ 003_copy/hjhlw/mycp.c
@@ -0,0 +1,58 @@
+#include<unistd.h>
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<string.h>
+
+int main(int argc,char *argv[])
+{
+    if(argc!=3)
+    {
+        printf("your parameters are incorrect\n");
+        return -1;
+    }
+
+    int src=open(argv[1],O_RDONLY);
+    if(src==-1)
+    {
+        printf("Error : reading the file\n");
+        perror("Error ");
+        return -1;
+    }
+    int dst=open(argv[2],O_WRONLY|O_CREAT,0666);
+    if(dst==-1)
+    {
+        printf("Error : creating the file\n");
+        perror("Error ");
+        return -1;
+
+    }
+
+    char buf[BUFSIZ]="";
+    int count=0;
+    while(1)
+    {
+        count=read(src,buf,BUFSIZ);
+        if(count==0)
+            break;
+        else if(count==-1)
+        {
+            perror("Error ");
+            break;
+            
+        }
+        else
+        {
+           count= write(dst,buf,count);
+           if(count==-1)
+           {
+               perror("Error ");
+               break;
+           }
+        }
+    }
+    close(src);
+    close(dst);
+    return 0;
+}
