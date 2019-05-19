3d089666571ee2ecff141fbc0213aa1f8a58e424
zkangHUST <1473345713@qq.com>
2019-05-19 12:39:55 +0800

    修改变量命名

--- 003_copy/vincent/mycp.c
+++ 003_copy/vincent/mycp.c
@@ -7,30 +7,30 @@ int main(int argc, char* argv[])
 {
     char            buf[BUFSIZE];
     int             cnt;
-    int             fdIn, fdOut;
+    int             fd_in, fd_out;
 
     if (argc < 3) {
         printf("Usage: mycp SOURCE DEST\n");   
         return -1;
     }
 
-    if ((fdIn = open(argv[1], O_RDONLY)) < 0) {
+    if ((fd_in = open(argv[1], O_RDONLY)) < 0) {
         perror("open");
         return -1;
     }
 
-    if ((fdOut = open(argv[2], O_WRONLY|O_CREAT, 0666)) < 0) {
+    if ((fd_out = open(argv[2], O_WRONLY|O_CREAT, 0666)) < 0) {
         perror("open");
         return -1;
     }
 
-    while ((cnt = read(fdIn, buf, BUFSIZE)) > 0) {
-        if(write(fdOut, buf, cnt) != cnt) {
+    while ((cnt = read(fd_in, buf, BUFSIZE)) > 0) {
+        if(write(fd_out, buf, cnt) != cnt) {
             perror("write");
         }
     } 
 
-    close(fdIn);
-    close(fdOut);
+    close(fd_in);
+    close(fd_out);
     return 0;
 }
