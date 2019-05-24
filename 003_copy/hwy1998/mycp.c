79b0e3864b4069d476a0f1e6f1811f0a30a36580
hwy1998 <1183598761@qq.com>
2019-05-24 15:54:32 +0800

    task3

---
+++ 003_copy/hwy1998/mycp.c
@@ -0,0 +1,38 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#define BUFFSIZE 8192
+
+int main(int argc, char** argv) {
+	if(argc == 3) {
+		int srcfd = open(argv[1], O_RDONLY);
+		int dstfd = open(argv[2], O_CREAT | O_WRONLY, 0666);
+		if(srcfd == -1) {
+			perror("Open:");
+			return 1;
+		}
+		if(dstfd == -1) {
+			perror("Open:");
+			return 1;
+		}
+		int size = 0;
+		char buff[BUFFSIZE] = {0};
+		while((size = read(srcfd, buff, BUFFSIZE))) {
+			if(write(dstfd, buff, BUFFSIZE) != size) {
+				perror("Write:");
+				return 1;
+			}
+		}
+		if(size < 0){
+			perror("Read:");
+			return 1;
+		}
+		close(srcfd);
+		close(dstfd);
+	}
+	else {
+		printf("Usage: .//mycp src dst");
+	}
+}
