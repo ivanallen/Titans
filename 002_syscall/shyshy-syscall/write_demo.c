982412a73b2f73410afd8e099c94168d8476a3ce
shy <1211646704@qq.com>
2019-05-24 16:37:17 +0800

    Merge branch 'dev' updated

--- 002_syscall/shyshy-syscall/write_demo.c
+++ 002_syscall/shyshy-syscall/write_demo.c
@@ -1,14 +1,16 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main(){
-   const char x[] = "shyshy903\n";
+int main() {
+    char x[] = "shyshy903!\n";
 
-#if defined(__x86_x64__)||defined(__amd64__)
-    __asm__("movq $1 ,%%rax;\n"
-            "movq $1 ,%%rdi;\n"
-            "movq %0 ,%%rsi;\n"
-            "movq $12,%%rdx;\n"
-            "syscall"::"r"(x):"%rax","%rdi","%rsi","%rdx");
+#if defined(__amd64__) || defined(_x86_x64_)
+    __asm__ ("movq $1, %%rax;\n"
+           "movq $1, %%rdi;\n"
+           "movq %0, %%rsi;\n"
+           "movq $12, %%rdx;\n"
+           "syscall"::"r"(x):"%rax", "%rdi", "%rsi", "%rdx");
+#else
+    printf("Unsupported Platform\n");
 #endif
     return 0;
 }
