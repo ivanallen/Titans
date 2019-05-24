66343cfd03712780df300886bed26ca43e53ab31
hwy1998 <1183598761@qq.com>
2019-05-24 18:04:58 +0800

    changed

--- 003_copy/hwy1998/mycp.c
+++ 003_copy/hwy1998/mycp.c
@@ -3,36 +3,35 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdio.h>
-#define BUFFSIZE 8192
+#define BUFFSIZE 2048
 
-int main(int argc, char** argv) {
-	if(argc == 3) {
+int main(int argc, char** argv){
+	if (argc == 3){
 		int srcfd = open(argv[1], O_RDONLY);
 		int dstfd = open(argv[2], O_CREAT | O_WRONLY, 0666);
-		if(srcfd == -1) {
+		if (srcfd == -1){
 			perror("Open:");
 			return 1;
 		}
-		if(dstfd == -1) {
+		if (dstfd == -1){
 			perror("Open:");
 			return 1;
 		}
 		int size = 0;
 		char buff[BUFFSIZE] = {0};
-		while((size = read(srcfd, buff, BUFFSIZE))) {
-			if(write(dstfd, buff, BUFFSIZE) != size) {
+		while ((size = read(srcfd, buff, BUFFSIZE))){
+			if (write(dstfd, buff, size) != size){
 				perror("Write:");
 				return 1;
 			}
 		}
-		if(size < 0){
+		if (size < 0){
 			perror("Read:");
 			return 1;
 		}
 		close(srcfd);
 		close(dstfd);
-	}
-	else {
+	}else {
 		printf("Usage: .//mycp src dst");
 	}
 }
