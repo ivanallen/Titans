80e1f9bfdee1af5e1ba90d665059a894d3b51497
Jacob <1515916596@qq.com>
2019-05-25 11:17:51 +0800

    commit task3

---
+++ 003_copy/skx/mycp.c
@@ -0,0 +1,42 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+#define MAX 1024
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        printf("parameter error\n");
+        return -1;
+    }
+    int src_fd = open(argv[1], O_RDONLY);
+    if (src_fd < 0) {
+        perror("Source file open failed");
+        return -1;
+    }
+    int dst_fd = open(argv[2], O_CREAT | O_WRONLY, 0666);
+    if (dst_fd == -1) {
+        perror("Destination file open failed");
+        return -1;
+    }
+    char buf[MAX];
+    int count=0;
+    while (1) {
+        count = read(src_fd, buf, MAX);
+        if (count == 0)
+        	break;
+        else if (count < 0) {
+            perror("read error");
+            break;
+        }
+        if (write(dst_fd, buf, count) != count) {
+            perror("write error");
+            break;
+        }
+    }
+    close(src_fd);
+    close(dst_fd);
+    if(count!=0)
+    	return -1;
+    return 0;
+}
+
